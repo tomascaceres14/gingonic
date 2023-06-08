1c5317ba5ffd89fb7ac4e1c7e15f00b7e5bccb94
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-06-07 21:47:35 -0300

    clase 6/7 productos supermercados

--- main.go
+++ main.go
@@ -2,42 +2,66 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
+	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-type product struct {
-	Name         string
-	Price        int
-	Stock        int
-	Code         int
-	Published    bool
-	CreationDate string
+type Product struct {
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Quantity    int     `json:"quantity"`
+	CodeValue   int     `json:"code_value"`
+	IsPublished bool    `json:"is_published"`
+	Expiration  string  `json:"expiration"`
+	Price       float64 `json:"price"`
 }
 
+var products []Product
+
 func main() {
+	loadProducts("products.json")
 	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) { c.String(http.StatusOK, "pong") })
 
-	data, err := os.ReadFile("./productos.json")
-	if err != nil {
-		log.Fatal(err)
+	productsGroup := router.Group("/products")
+	{
+		productsGroup.GET("/search", Search)
 	}
-
-	var products []product
-	if err := json.Unmarshal(data, &products); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(products)
-
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"products": products,
-		})
-	})
 
 	router.Run()
+}
+
+func loadProducts(path string) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = json.Unmarshal([]byte(file), &products); err != nil {
+		panic(err)
+	}
+}
+
+func Search(c *gin.Context) {
+
+	query := c.Query("priceGt")
+	priceGt, err := strconv.ParseFloat(query, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid price",
+		})
+		return
+	}
+
+	list := []Product{}
+	for _, v := range products {
+		if v.Price > priceGt {
+			list = append(list, v)
+		}
+	}
+
+	c.JSON(http.StatusOK, list)
 }
